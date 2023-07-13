cf6a2f51af66dfcfc30bc2465a87c302a385d1b9
Emanueldiv1 <fergimanu3@gmail.com>
2023-07-13 12:59:56 -0300

    Domain model

---
+++ src/main/java/com/devSuperior/dslist/entities/Belonging.java
@@ -0,0 +1,60 @@
+package com.devSuperior.dslist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+
+	@EmbeddedId
+	private BelongingPk id = new BelongingPk();
+	
+	private Integer position;
+	
+	public Belonging() {
+	}
+
+	public Belonging(Game game, GameList list, Integer position ) {
+		id.setGame(game);
+		id.setList(list);
+		this.position = position;
+	}
+
+	public BelongingPk getId() {
+		return id;
+	}
+
+	public void setId(BelongingPk id) {
+		this.id = id;
+	}
+
+	public Integer getPosition() {
+		return position;
+	}
+
+	public void setPosition(Integer position) {
+		this.position = position;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Belonging other = (Belonging) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
---
+++ src/main/java/com/devSuperior/dslist/entities/BelongingPk.java
@@ -0,0 +1,61 @@
+package com.devSuperior.dslist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class BelongingPk {
+
+	@ManyToOne
+	@JoinColumn(name = "game_id")
+	private Game game;
+	@ManyToOne
+	@JoinColumn(name = "list_id")
+	private GameList list;	
+	
+	public BelongingPk() {
+	}
+
+	public BelongingPk(Game game, GameList list) {
+		this.game = game;
+		this.list = list;
+	}
+
+	public Game getGame() {
+		return game;
+	}
+
+	public void setGame(Game game) {
+		this.game = game;
+	}
+
+	public GameList getList() {
+		return list;
+	}
+
+	public void setList(GameList list) {
+		this.list = list;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(game, list);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BelongingPk other = (BelongingPk) obj;
+		return Objects.equals(game, other.game) && Objects.equals(list, other.list);
+	}
+	
+	
+}
