8f7b48e63fb738af14f38a918ff17bf71469d62a
Emanueldiv1 <fergimanu3@gmail.com>
2023-07-18 22:25:41 -0300

    move game on list

--- src/main/java/com/devSuperior/dslist/controller/GameListController.java
+++ src/main/java/com/devSuperior/dslist/controller/GameListController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devSuperior.dslist.dto.GameListDTO;
 import com.devSuperior.dslist.dto.GameMinDTO;
+import com.devSuperior.dslist.dto.ReplacementDTO;
 import com.devSuperior.dslist.services.GameListService;
 import com.devSuperior.dslist.services.GameService;
 
@@ -34,4 +37,10 @@ public class GameListController {
 		List<GameMinDTO> result = gameService.findByList(listId);
 		return result;
 	}
+	
+	@PostMapping(value = ("/{listId}/replacement") )
+	public void move( @PathVariable Long listId, @RequestBody ReplacementDTO body){
+		gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex()); 
+	}
+	
 }
--- src/main/java/com/devSuperior/dslist/services/GameListService.java
+++ src/main/java/com/devSuperior/dslist/services/GameListService.java
@@ -2,13 +2,17 @@ package com.devSuperior.dslist.services;
 
 import java.util.List;
 
+import javax.xml.transform.Source;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devSuperior.dslist.Projections.GameMinProjection;
 import com.devSuperior.dslist.dto.GameListDTO;
 import com.devSuperior.dslist.entities.GameList;
 import com.devSuperior.dslist.repositores.GameListRepository;
+import com.devSuperior.dslist.repositores.GameRepository;
 
 
 
@@ -18,11 +22,32 @@ public class GameListService {
 	@Autowired
 	private GameListRepository gameListRepository;
 	
+	@Autowired
+	private GameRepository gameRepository;
+	
 	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll(){
 		List <GameList> result = gameListRepository.findAll();
 		return result.stream().map(x -> new GameListDTO(x)).toList(); 
+	}	
+	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
 		
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		
+		GameMinProjection obj = list.remove(sourceIndex);
+		
+		list.add(destinationIndex, obj);  
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex; 
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex; 
+		 
+		for(int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+			
+		}
 	}
+	
 }
